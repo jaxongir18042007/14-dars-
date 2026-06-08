ab41c2dc3bb34a8012b4908907f6b8d6ad1d4194
Jahongir <jaxongir2565@gmail.com>
2026-06-09 00:40:09 +0500

    yangilash

--- java.js
+++ java.js
@@ -31,6 +31,28 @@ box5.onclick = () =>{
 box6.onclick = () =>{
     rasm.src = "turkey.jpg";
 }
-kuntun.onclick = () =>{
-    document.querySelector('.asosiyquti').classList.toggle('keyin');
+kuntun.onclick = () => {
+    document.querySelector('.asosiyquti').classList.toggle('keyin1');
+    document.querySelector('#markaziyquti').classList.toggle('keyin2');
+    document.querySelector('#fortext').classList.toggle('keyin3');
+    document.querySelector('#kuntun').classList.toggle('keyin4');
+    document.querySelector('#tugma').classList.toggle('keyin5');
+    document.querySelector('#royhatlar').classList.toggle('keyin6');
+    document.querySelector('#box1').classList.toggle('keyin7');
+    document.querySelector('#box2').classList.toggle('keyin8');
+    document.querySelector('#box3').classList.toggle('keyin9');
+    document.querySelector('#box4').classList.toggle('keyin10');
+    document.querySelector('#box5').classList.toggle('keyin11');
+    document.querySelector('#box6').classList.toggle('keyin12');
+    document.querySelector('#rasm').classList.toggle('keyin13');
+    document.querySelector('body').classList.toggle('keyin14');
+    document.querySelector('#pastkichiziq').classList.toggle('keyin15');
+
+
+
+
+
+
+
 }
+

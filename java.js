ac027e795d08de5e6be812b7af7adc770838bb7b
Jahongir <jaxongir2565@gmail.com>
2026-06-13 11:36:55 +0500

    yangilash

--- java.js
+++ java.js
@@ -1,3 +1,45 @@
-asosiyquti.onclick = () => {
-    document.querySelector('#asosiyquti').classList.toggle('keyin1');
+markaziy3.onclick = () => {
+    document.querySelector('#markaziy3').classList.toggle('keyin3');
+    document.querySelector('#markaziy1').classList.toggle('keyin1');
+    document.querySelector('#markaziy2').classList.toggle('keyin2');
+    document.querySelector('#markaziy4').classList.toggle('keyin4');
+    document.querySelector('#tayoqcha').classList.toggle('keyin5');
+    document.querySelector('#fortext').classList.toggle('keyin6');
+    document.querySelector('#t2').classList.toggle('keyin7');
+}
+markaziy1.onclick = () => {
+    document.querySelector('#markaziy3').classList.toggle('keyin3');
+    document.querySelector('#markaziy1').classList.toggle('keyin1');
+    document.querySelector('#markaziy2').classList.toggle('keyin2');
+    document.querySelector('#markaziy4').classList.toggle('keyin4');
+    document.querySelector('#tayoqcha').classList.toggle('keyin5');
+    document.querySelector('#fortext').classList.toggle('keyin6');
+    document.querySelector('#t2').classList.toggle('keyin7');
+}
+markaziy2.onclick = () => {
+    document.querySelector('#markaziy3').classList.toggle('keyin3');
+    document.querySelector('#markaziy1').classList.toggle('keyin1');
+    document.querySelector('#markaziy2').classList.toggle('keyin2');
+    document.querySelector('#markaziy4').classList.toggle('keyin4');
+    document.querySelector('#tayoqcha').classList.toggle('keyin5');
+    document.querySelector('#fortext').classList.toggle('keyin6');
+    document.querySelector('#t2').classList.toggle('keyin7');
+}
+markaziy4.onclick = () => {
+    document.querySelector('#markaziy3').classList.toggle('keyin3');
+    document.querySelector('#markaziy1').classList.toggle('keyin1');
+    document.querySelector('#markaziy2').classList.toggle('keyin2');
+    document.querySelector('#markaziy4').classList.toggle('keyin4');
+    document.querySelector('#tayoqcha').classList.toggle('keyin5');
+    document.querySelector('#fortext').classList.toggle('keyin6');
+    document.querySelector('#t2').classList.toggle('keyin7');
+}
+tayoqcha.onclick = () => {
+    document.querySelector('#markaziy3').classList.toggle('keyin3');
+    document.querySelector('#markaziy1').classList.toggle('keyin1');
+    document.querySelector('#markaziy2').classList.toggle('keyin2');
+    document.querySelector('#markaziy4').classList.toggle('keyin4');
+    document.querySelector('#tayoqcha').classList.toggle('keyin5');
+    document.querySelector('#fortext').classList.toggle('keyin6');
+    document.querySelector('#t2').classList.toggle('keyin7');
 }
